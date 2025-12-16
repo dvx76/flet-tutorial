96d779e67cd07fb357250422c69e018b3a3307d4
Fabrice Devaux <fabrice.devaux@gmail.com>
2025-12-16 09:26:22 +0100

    Main branch is used for Todo app code

--- src/main.py
+++ src/main.py
@@ -1,193 +1,144 @@
-from typing import Callable, Optional
+from typing import Callable
 
 import flet as ft
 
-from flaskrclient import BlogPost, FlaskrClient
 
-
-class PostView(ft.Column):
+class Task(ft.Column):
     def __init__(
-        self,
-        post: BlogPost,
-        on_edit: Optional[Callable] = None,
-        on_delete: Optional[Callable] = None,
+        self, task_text: str, task_delete: Callable, task_status_change: Callable
     ):
         super().__init__()
-        self.post = post
-        self.on_edit = on_edit
-        self.on_delete = on_delete
+        self.completed: bool = False
+        self.task_text = task_text
+        self.task_status_change = task_status_change
+        self.task_delete = task_delete
+        self.display_task = ft.Checkbox(
+            value=False,
+            label=self.task_text,
+            on_change=self.status_changed,
+            expand=True,
+        )
+        self.edit_name = ft.TextField(expand=True)
 
-        self.title = ft.Text(
-            self.post.title, theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM
+        self.display_view = ft.Row(
+            controls=[
+                ft.Container(
+                    content=self.display_task,
+                    expand=True,
+                    clip_behavior=ft.ClipBehavior.HARD_EDGE,
+                ),
+                ft.Row(
+                    controls=[
+                        ft.IconButton(
+                            icon=ft.Icons.CREATE_OUTLINED,
+                            tooltip="Edit To-Do",
+                            on_click=self.edit_clicked,
+                        ),
+                        ft.IconButton(
+                            icon=ft.Icons.DELETE_OUTLINE,
+                            icon_color=ft.Colors.RED,
+                            tooltip="Delete To-Do",
+                            on_click=self.delete_clicked,
+                        ),
+                    ],
+                ),
+            ],
         )
-        self.subtitle = ft.Text(
-            f"by {self.post.author} on {self.post.created}",
-            theme_style=ft.TextThemeStyle.LABEL_SMALL,
-        )
-        self.body = ft.Text(self.post.body)
-        interaction_buttons = ft.Row()
-        if self.on_edit:
-            interaction_buttons.controls.append(
-                ft.IconButton(icon=ft.Icons.EDIT, on_click=lambda e: self.on_edit(self))
-            )
-        if self.on_delete:
-            interaction_buttons.controls.append(
+
+        self.edit_view = ft.Row(
+            visible=False,
+            controls=[
+                self.edit_name,
                 ft.IconButton(
-                    icon=ft.Icons.DELETE,
-                    icon_color=ft.Colors.RED_300,
-                    on_click=lambda e: self.on_delete(self),
-                )
-            )
-        self.controls = [
-            ft.Row(controls=[self.title, interaction_buttons]),
-            self.subtitle,
-            self.body,
-            ft.Divider(),
-        ]
-
-    def update(self):
-        self.title.value = self.post.title
-        self.body.value = self.post.body
-        super().update()
-
-
-class FlaskrApp(ft.Column):
-    def __init__(self, client: FlaskrClient):
-        super().__init__()
-        self.client = client
-        self.username = ""
-        self.new_post_button = ft.ElevatedButton(
-            text="New Post", on_click=self.new_post, disabled=True
+                    icon=ft.Icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=ft.Colors.GREEN,
+                    tooltip="Update To-Do",
+                    on_click=self.save_clicked,
+                ),
+            ],
         )
-        self.posts_list_view = ft.Column()
+        self.controls = [self.display_view, self.edit_view]
+
+    def edit_clicked(self, e):
+        self.edit_name.value = self.display_task.label
+        self.display_view.visible = False
+        self.edit_view.visible = True
+        self.update()
+
+    def save_clicked(self, e):
+        self.display_task.label = self.edit_name.value
+        self.display_view.visible = True
+        self.edit_view.visible = False
+        self.update()
+
+    def delete_clicked(self, e):
+        self.task_delete(self)
+
+    def status_changed(self, e):
+        self.completed = self.display_task.value
+        self.task_status_change()
+
+
+class TodoApp(ft.Column):
+    def __init__(self):
+        super().__init__()
+        self.new_task = ft.TextField(
+            hint_text="What needs to be done?", on_submit=self.add_clicked, expand=True
+        )
+        self.tasks = ft.Column()
+        self.tabs = ft.Tabs(
+            scrollable=False,
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[ft.Tab(text="all"), ft.Tab(text="active"), ft.Tab(text="completed")],
+        )
+        self.width = 600
         self.controls = [
             ft.Row(
                 controls=[
-                    ft.Text(
-                        value="FlaskR Blog",
-                        theme_style=ft.TextThemeStyle.HEADLINE_LARGE,
+                    self.new_task,
+                    ft.FloatingActionButton(
+                        icon=ft.Icons.ADD, on_click=self.add_clicked
                     ),
-                    ft.IconButton(icon=ft.Icons.SETTINGS, on_click=self.enter_creds),
-                ],
+                ]
             ),
-            self.new_post_button,
-            self.posts_list_view,
+            self.tabs,
+            self.tasks,
         ]
 
-    def refresh(self):
-        postview_list = []
-        for post in self.client.posts():
-            if self.client.authenticated and self.username == post.author:
-                postview_list.append(
-                    PostView(
-                        post=post, on_edit=self.edit_post, on_delete=self.delete_post
-                    )
-                )
-            else:
-                postview_list.append(PostView(post=post))
-        self.posts_list_view.controls = postview_list
+    def add_clicked(self, e):
+        task = Task(
+            task_text=self.new_task.value,
+            task_delete=self.task_delete,
+            task_status_change=self.update,
+        )
+        self.tasks.controls.append(task)
+        self.new_task.value = ""
+        self.new_task.focus()
         self.update()
 
-    def enter_creds(self, e):
-        def set_creds(e):
-            self.username = str(username_field.value)
-            self.client.set_auth(self.username, str(password_field.value))
-            self.new_post_button.disabled = False
-            self.refresh()
-            self.page.close(dialog)
+    def task_delete(self, task: Task):
+        self.tasks.controls.remove(task)
+        self.update()
 
-        username_field = ft.TextField(label="Username", autofocus=True)
-        password_field = ft.TextField(label="Password", password=True)
-        dialog = ft.AlertDialog(
-            title=ft.Text("API Credentials"),
-            content=ft.Column([username_field, password_field], tight=True),
-            actions=[
-                ft.ElevatedButton("Cancel", on_click=lambda _: self.page.close(dialog)),
-                ft.ElevatedButton("Save", on_click=set_creds),
-            ],
-            modal=True,
-        )
-        self.page.open(dialog)
+    def tabs_changed(self, e):
+        self.update()
 
-    def new_post(self, e):
-        def submit_post(e):
-            post = self.client.new_post(
-                title=str(title_field.value), body=str(body_field.value)
+    def before_update(self):
+        status = self.tabs.tabs[self.tabs.selected_index].text
+        for task in self.tasks.controls:
+            task.visible = (
+                status == "all"
+                or (status == "active" and not task.completed)
+                or (status == "completed" and task.completed)
             )
-            self.posts_list_view.controls.insert(0, PostView(post))
-            self.update()
-            self.page.close(dialog)
-
-        title_field = ft.TextField(label="Title", autofocus=True, max_length=100)
-        body_field = ft.TextField(
-            label="Body", multiline=True, min_lines=5, max_lines=10, max_length=5000
-        )
-
-        dialog = ft.AlertDialog(
-            title=ft.Text("New Post"),
-            content=ft.Column([title_field, body_field], tight=True, width=500),
-            actions=[
-                ft.ElevatedButton("Cancel", on_click=lambda _: self.page.close(dialog)),
-                ft.ElevatedButton("Submit", on_click=submit_post),
-            ],
-        )
-        self.page.open(dialog)
-
-    def edit_post(self, postview: PostView):
-        def submit_post(e):
-            title = str(title_field.value)
-            body = str(body_field.value)
-            post = self.client.edit_post(
-                post_id=postview.post.id, title=title, body=body
-            )
-            self.page.close(dialog)
-            postview.post = post
-            postview.update()
-
-        title_field = ft.TextField(
-            value=postview.post.title, label="Title", autofocus=True, max_length=100
-        )
-        body_field = ft.TextField(
-            value=postview.post.body,
-            label="Body",
-            multiline=True,
-            min_lines=5,
-            max_lines=10,
-            max_length=5000,
-        )
-
-        dialog = ft.AlertDialog(
-            title=ft.Text("Edit Post"),
-            content=ft.Column([title_field, body_field], tight=True, width=500),
-            actions=[
-                ft.ElevatedButton("Cancel", on_click=lambda _: self.page.close(dialog)),
-                ft.ElevatedButton("Submit", on_click=submit_post),
-            ],
-        )
-        self.page.open(dialog)
-
-    def delete_post(self, postview: PostView):
-        def delete_post(e):
-            self.client.delete_post(post_id=postview.post.id)
-            self.page.close(dialog)
-            self.posts_list_view.controls.remove(postview)
-            self.posts_list_view.update()
-
-        dialog = ft.AlertDialog(
-            title=ft.Text(f"Delete '{postview.post.title}'?"),
-            actions=[
-                ft.ElevatedButton("Cancel", on_click=lambda _: self.page.close(dialog)),
-                ft.ElevatedButton("Delete", on_click=delete_post),
-            ],
-        )
-        self.page.open(dialog)
 
 
 def main(page: ft.Page):
-    client = FlaskrClient()
-    flaskr_app = FlaskrApp(client=client)
-    page.add(flaskr_app)
-    flaskr_app.refresh()
+    page.title = "To-Do App"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(TodoApp())
+    page.update()
 
 
-ft.app(target=main)
+ft.app(main)
